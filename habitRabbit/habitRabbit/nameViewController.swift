4a46890e86a43fe94c04c9a2dc117456f81e48cd
KAbamonga <87456353+KAbamonga@users.noreply.github.com>
2021-07-22 12:02:20 -0400

    Version 2.0
    Kiara - July 22 12:02 PM

---
+++ habitRabbit/habitRabbit/nameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  nameViewController.swift
+//  habitRabbit
+//
+//  Created by Kiara Abamonga on 7/22/21.
+//
+
+import UIKit
+
+class nameViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextBox: UITextField!
+    
+    @IBOutlet weak var displayName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        displayName.text = nameTextBox.text
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func enterName(_ sender: UIButton) {
+        displayName.text = "🌸 Welcome, " +  nameTextBox.text! + "! 🌸"
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
